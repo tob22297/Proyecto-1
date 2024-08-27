03c2991169939ce714e60d1889031fb89b8b82fb
tob22297 <tob22297@uvg.edu.gt>
2024-08-27 11:20:15 -0600

    Create config.h

---
+++ config.h
@@ -0,0 +1,14 @@
+//Conecta el adafruit
+#define IO_USERNAME  "tob22297"
+#define IO_KEY       "aio_BReS367KCW5stxQ4U0IZjkx2CnLh"
+
+#define WIFI_SSID "iPhone de Marcela"
+#define WIFI_PASS "marce2003"
+
+
+#include "AdafruitIO_WiFi.h"
+
+
+
+
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID,WIFI_PASS);
